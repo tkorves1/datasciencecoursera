f0deb161ef9d7b9091d9b08931ddffe83b5f52e4
tkorves1 <tonia.korves@gmail.com>
2014-09-21 12:43:39 -0400

    CodeBook.md

---
+++ CodeBook.md
@@ -0,0 +1,96 @@
+##Codebook- Human Activity Recognition Using Smartphones Dataset
+Means for Measurements by Activity and Subject
+
+##1 Measurement
+
+### The measured features.  There are four kinds of features measured:  Body Acceleration Signal (BodyAccelerationSignal), Body Acceleration Jerk Component (BodyAccelJerkSignal), Body Acceleration Gravity Component (BodyAccelGyroSignal), and Gravity Acceleration Signal (GravityAccelerationSignal)
+##A leading t indicates a feature with a time domain measurement; these were not Fourier transformed
+### A leading f indicates that the measurement variable is Fast Fourier Transformed 
+### Mag is short for magnitude
+### X, Y, Z indicates that the measurement is a raw signal in the X, Y , or Z axis
+### mean() indicates that the base feature is a mean of measurements
+### std() indicates that the base feature is a standard deviation of measurements
+ 
+* "1" "fBodyAccelerationSignal-mean()-X"
+* "2" "fBodyAccelerationSignal-mean()-Y"
+* "3" "fBodyAccelerationSignal-mean()-Z"
+* "4" "fBodyAccelerationSignal-std()-X"
+* "5" "fBodyAccelerationSignal-std()-Y"
+* "6" "fBodyAccelerationSignal-std()-Z"
+* "7" "fBodyAccelJerkSignal-mean()-X"
+* "8" "fBodyAccelJerkSignal-mean()-Y"
+* "9" "fBodyAccelJerkSignal-mean()-Z"
+* "10" "fBodyAccelJerkSignal-std()-X"
+* "11" "fBodyAccelJerkSignal-std()-Y"
+* "12" "fBodyAccelJerkSignal-std()-Z"
+* "13" "fBodyAccMag-mean()"
+* "14" "fBodyAccMag-std()"
+* "15" "fBodyBodyAccJerkMag-mean()"
+* "16" "fBodyBodyAccJerkMag-std()"
+* "17" "fBodyBodyGyroJerkMag-mean()"
+* "18" "fBodyBodyGyroJerkMag-std()"
+* "19" "fBodyBodyGyroMag-mean()"
+* "20" "fBodyBodyGyroMag-std()"
+* "21" "fBodyAccelGyroSignal-mean()-X"
+* "22" "fBodyAccelGyroSignal-mean()-Y"
+* "23" "fBodyAccelGyroSignal-mean()-Z"
+* "24" "fBodyAccelGyroSignal-std()-X"
+* "25" "fBodyAccelGyroSignal-std()-Y"
+* "26" "fBodyAccelGyroSignal-std()-Z"
+* "27" "tBodyAccelerationSignal-mean()-X"
+* "28" "tBodyAccelerationSignal-mean()-Y"
+* "29" "tBodyAccelerationSignal-mean()-Z"
+* "30" "tBodyAccelerationSignal-std()-X"
+* "31" "tBodyAccelerationSignal-std()-Y"
+* "32" "tBodyAccelerationSignal-std()-Z"
+* "33" "tBodyAccelJerkSignal-mean()-X"
+* b"34" "tBodyAccelJerkSignal-mean()-Y"
+* "35" "tBodyAccelJerkSignal-mean()-Z"
+* "36" "tBodyAccelJerkSignal-std()-X"
+* "37" "tBodyAccelJerkSignal-std()-Y"
+* "38" "tBodyAccelJerkSignal-std()-Z"
+* "39" "tBodyAccJerkMag-mean()"
+* "40" "tBodyAccJerkMag-std()"
+* "41" "tBodyAccMag-mean()"
+* "42" "tBodyAccMag-std()"
+* "43" "tBodyAccelGyroSignal-mean()-X"
+* "44" "tBodyAccelGyroSignal-mean()-Y"
+* "45" "tBodyAccelGyroSignal-mean()-Z"
+* "46" "tBodyAccelGyroSignal-std()-X"
+* "47" "tBodyAccelGyroSignal-std()-Y"
+* "48" "tBodyAccelGyroSignal-std()-Z"
+* "49" "tBodyGyroJerk-mean()-X"
+* "50" "tBodyGyroJerk-mean()-Y"
+* "51" "tBodyGyroJerk-mean()-Z"
+* "52" "tBodyGyroJerk-std()-X"
+* "53" "tBodyGyroJerk-std()-Y"
+* "54" "tBodyGyroJerk-std()-Z"
+* "55" "tBodyGyroJerkMag-mean()"
+* "56" "tBodyGyroJerkMag-std()"
+* "57" "tBodyGyroMag-mean()"
+* "58" "tBodyGyroMag-std()"
+* "59" "tGravityAccelerationSignal-mean()-X"
+* "60" "tGravityAccelerationSignal-mean()-Y"
+* "61" "tGravityAccelerationSignal-mean()-Z"
+* "62" "tGravityAccelerationSignal-std()-X"
+* "63" "tGravityAccelerationSignal-std()-Y"
+* "64" "tGravityAccelerationSignal-std()-Z"
+* "65" "tGravityAccMag-mean()"
+* "66" "tGravityAccMag-std()"
+
+## Activity
+* 1 WALKING
+* 2 WALKING_UPSTAIRS
+* 3 WALKING_DOWNSTAIRS
+* 4 SITTING
+* 5 STANDING
+* 6 LAYING
+
+
+## Subject
+* Subject person number, 1 to 30
+
+## Mean
+* Mean of feature for the activity and subject listed
+* The initial measures were time domain measures, from which means or standard deviations were taken. These were then normalized.  These normalized values are averaged for activity and subject here.   
+
---
+++ run_analysis.R
@@ -0,0 +1,138 @@
+#This script creates some summary data from the dataset called "Human Activity Recognition Using Smartphones Dataset".  It selects the mean and standard deviation measurements for each of the features measured in the dataset and calculates means for each of these for each activity-human subject pair.
+
+
+# The website for this dataset is http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+#Download the data into dfs
+
+#setwd to your working directory
+
+fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if(! file.exists("Cdata")){dir.create("Cdata")
+}
+temp<-tempfile()
+download.file(fileUrl,temp)
+for (i in temp){
+  unzip(i,exdir="./Cdata")
+}
+#list.files("./Cdata")
+
+###start here for new work
+
+testData<-read.table("./Cdata/UCI HAR Dataset/test/X_test.txt",header=FALSE)
+trainingData<-read.table("./Cdata/UCI HAR Dataset/train/X_train.txt",header=FALSE)
+
+features<-read.table("./Cdata/UCI HAR Dataset/features.txt")
+featureNames<-as.character(features$V2)
+
+colnames(testData)<-c(featureNames)
+colnames(trainingData)<-c(featureNames)
+
+#Reduce Data sets to mean and std of features only
+
+features1<-data.frame(featureNames,stringsAsFactors=FALSE)
+
+#Get true/false on whether contain mean, std
+#"mean ()" is used so that variable names with meanFreq, which I don't think should be considered, are not included
+
+features1$mean<-grepl(("mean()"),features1$featureNames)
+features1$meanFreq<-grepl(("meanFreq()"),features1$featureNames)
+features1$std<-grepl(("std"),features1$featureNames)
+
+#include those where either true
+
+featuresToInclude<-features1$featureNames[(features1$mean==TRUE|features1$std==TRUE)& features1$meanFreq==FALSE]
+
+#Reduce data set to colms in this set where contain mean and std
+
+testData2<-testData[,colnames(testData) %in% featuresToInclude]
+trainingData2<-trainingData[,colnames(trainingData) %in% featuresToInclude]
+
+
+#read in activity data labels
+
+trainingActLabels<-read.table("./Cdata/UCI HAR Dataset/train/y_train.txt",header=FALSE)
+testActLabels<-read.table("./Cdata/UCI HAR Dataset/test/y_test.txt",header=FALSE)
+
+#Add order to these so that can reorder correctly after merge to line up with measurement data
+trainingActLabels$order<-1:nrow(trainingActLabels)
+testActLabels$order<-1:nrow(testActLabels)
+
+
+#Read in file that has the descriptive names corresponding to the activity numbers
+
+ActivityLabels<-read.table("./Cdata/UCI HAR Dataset/activity_labels.txt",header=FALSE)
+colnames(ActivityLabels)[2]<-"Activity"
+
+
+
+#Merge activity names with activity label numbers
+trainingActLabelNames<-merge(trainingActLabels,ActivityLabels,by="V1")
+testActLabelNames<-merge(testActLabels,ActivityLabels,by="V1")
+
+#Reorder the Activity number according to the original order
+
+trainingActLabelNames<-trainingActLabelNames[order(trainingActLabelNames$order),]
+testActLabelNames<-testActLabelNames[order(testActLabelNames$order),]
+
+
+#Remove the order variable
+trainingActLabelNames<-trainingActLabelNames[,-2]
+testActLabelNames<-testActLabelNames[,-2]
+
+
+
+
+
+
+#obtain subject numbers
+
+trainingSubj<-read.table("./Cdata/UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+testSubj<-read.table("./Cdata/UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+colnames(testSubj)[1]<-"Subject"
+colnames(trainingSubj)[1]<-"Subject"
+
+#combine activity, subject and feature data
+
+testDatawLabels<-cbind(testActLabelNames[2],testSubj,testData2)
+trainingDatawLabels<-cbind(trainingActLabelNames[2],trainingSubj,trainingData2)
+
+#combine training and test data
+
+DataAll<-rbind(trainingDatawLabels,testDatawLabels)
+
+colnames(DataAll)[1]<-"Activity"
+colnames(DataAll)[2]<-"Subject"
+
+#Melt data into a dataframe with the measurements broken down on separate lines
+
+library(reshape2)
+
+mDataAll<-melt(DataAll,id=c("Activity","Subject"))
+
+
+#Calculate the mean for each measurement for each activity-subject pair
+
+library(plyr)
+meanset<-ddply(mDataAll,.(variable,Activity,Subject),summarise,Mean=mean(value))
+colnames(meanset)[1]<-"Measurement"
+
+#Reorder so dataset looks nice.
+meanset<-meanset[order(as.character(meanset$Measurement),as.character(meanset$Activity),meanset$Subject),]
+
+#Replace measurement names with more descriptive names
+
+
+meanset$Measurement<- sub("BodyAcc-","BodyAccelerationSignal-",meanset$Measurement)
+meanset$Measurement<- sub("GravityAcc-","GravityAccelerationSignal-",meanset$Measurement)
+meanset$Measurement<- sub("BodyAccJerk-","BodyAccelJerkSignal-",meanset$Measurement)
+meanset$Measurement<- sub("BodyGyro-","BodyAccelGyroSignal-",meanset$Measurement)
+
+#Write dataset to table
+
+write.table(meanset,file="GCRclassMeanSet.txt",row.name=FALSE) 
+
+
+#write.table(unique(meanset$Measurement),file="MeasurementList5.txt")
+#write.table(unique(meanset$Activity)),file="ActivityList.txt")
+
